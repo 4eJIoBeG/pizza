3becab843e6a4ff116880e4e5aa7542fd683c0f7
4eJIoBeG <pavelshashenko@gmail.com>
2023-11-23 02:06:48 +0300

    add interactive to cart, need fix counters in cart and remove items in cart bug

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,9 +1,33 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-function PizzaBlock({ title, price, imageUrl, types, sizes, category }) {
+import { addItem } from "../../redux/slices/cartSlice";
+
+const typesNames = ["тонкое", "традиционное"];
+
+function PizzaBlock({ id, title, price, imageUrl, types, sizes }) {
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) =>
+    state.cart.items.find((obj) => obj.id === id),
+  );
+
+  const addedCount = cartItem ? cartItem.count : 0;
+
   const [activeType, setActiveType] = useState(0);
   const [activeSize, setActiveSize] = useState(0);
-  const typesNames = ["тонкое", "традиционное"];
+
+  const onClickAdd = () => {
+    const item = {
+      id,
+      title,
+      price,
+      imageUrl,
+      type: typesNames[activeType],
+      size: sizes[activeSize],
+    };
+    dispatch(addItem(item));
+  };
+
   return (
     <div key={title} className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
@@ -34,7 +58,10 @@ function PizzaBlock({ title, price, imageUrl, types, sizes, category }) {
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <button className="button button--outline button--add">
+        <button
+          onClick={onClickAdd}
+          className="button button--outline button--add"
+        >
           <svg
             width="12"
             height="12"
@@ -48,7 +75,7 @@ function PizzaBlock({ title, price, imageUrl, types, sizes, category }) {
             />
           </svg>
           <span>Добавить</span>
-          <i>0</i>
+          {addedCount > 0 && <i>{addedCount}</i>}
         </button>
       </div>
     </div>
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -9,13 +9,44 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    setCategoryId(state, action) {
-      state.categoryId = action.payload;
+    addItem(state, action) {
+      const findItem = state.items.find(
+        (obj) =>
+          obj.id === action.payload.id &&
+          obj.size === action.payload.size &&
+          obj.type === action.payload.type,
+      );
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({ ...action.payload, count: 1 });
+      }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+    minusItem(state, action) {
+      const findItem = state.items.find(
+        (obj) =>
+          obj.id === action.payload.id &&
+          obj.size === action.payload.size &&
+          obj.type === action.payload.type,
+      );
+      if (findItem) {
+        findItem.count--;
+      }
+    },
+    removeItem(state, action) {
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+    },
+    clearItems(state) {
+      state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setCategoryId } = cartSlice.actions;
+export const { addItem, removeItem, clearItems, minusItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
